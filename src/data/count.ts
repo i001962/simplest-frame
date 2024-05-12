fd9c1a77a01181e3d6af0da9ab3611c542538e3c
i001962@gmail.com <i001962@gmail.com>
2024-05-12 12:28:03 -0700

    blobs error

--- src/data/count.ts
+++ src/data/count.ts
@@ -2,8 +2,9 @@ import { getStore } from '@netlify/blobs';
 
 const getCount = async() => {
     console.log('Getting count');
-    const store = getStore('gameState');
-    const rawCount = await store.get('count', {consistency: 'strong'});
+    //const store = getStore('gameState');
+    //const rawCount = await store.get('count', {consistency: 'strong'});
+    const rawCount = '0';
     console.log(`Raw count is ${rawCount}`);
     let count = parseInt(rawCount);
     if (Number.isNaN(count)) count = 0;
