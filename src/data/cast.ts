4976647e93b92d2385fafdf5d45df2c489e3537d
i001962@gmail.com <i001962@gmail.com>
2024-05-12 11:30:26 -0700

    debug msgs

--- src/data/cast.ts
+++ src/data/cast.ts
@@ -1,11 +1,12 @@
 import { streamToString } from '../../modules/utils.js';
 
 const getCast = async(castHash) => {
+    console.log(`Fetching cast with hash ${castHash}`);
     const request = await fetch(`https://protocol.wield.co/farcaster/v2/cast?hash=${castHash}`, {
         method: "GET",
         headers: { "API-KEY": process.env.WIELD_API_KEY }
     });
-
+    console.log(`Received response with status ${request.status}`);
     const body = await streamToString(request.body);
     const data = JSON.parse(body);
     return data.result.cast;
